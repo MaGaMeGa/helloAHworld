6c7abc9b43c9fc76f8b3355675ede22acbc187ac
MaGaMeGa <gabormajo@yahoo.com>
2021-03-19 13:34:29 +0100

    removed bash from certificate creation scripts

--- os/alpine/tests/clienttests/create_test_client_cert.sh
+++ os/alpine/tests/clienttests/create_test_client_cert.sh
@@ -17,9 +17,9 @@ IP_ADDRESS="127.0.0.1"
 CLOUD_NAME="testcloud2"
 CLOUD_ALIAS="testcloud2.aitia.arrowhead.eu"
 
-bash $PWD/genSystemCert.sh "${SYSTEM_NAME}" "${SYSTEM_PASS}" "${HOST_NAME}" "${IP_ADDRESS}" "${CLOUD_NAME}" "${CLOUD_ALIAS}"
+$PWD/genSystemCert.sh "${SYSTEM_NAME}" "${SYSTEM_PASS}" "${HOST_NAME}" "${IP_ADDRESS}" "${CLOUD_NAME}" "${CLOUD_ALIAS}"
 
 
 keytool -exportcert -alias  "${SYSTEM_NAME}" -keystore "${SYSTEM_NAME}.p12" -storepass "${SYSTEM_PASS}" -rfc | openssl x509 -out "${SYSTEM_NAME}.pub" -noout -pubkey
 
-bash $PWD/normalize_pub.sh "${SYSTEM_NAME}.pub"
+$PWD/normalize_pub.sh "${SYSTEM_NAME}.pub"
