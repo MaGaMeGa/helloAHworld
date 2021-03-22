127a5a0feed1b95e27d3fe8eece110ae0fe0bd79
MaGaMeGa <gabormajo@yahoo.com>
2021-03-22 19:57:23 +0000

    working clienttests

--- os/alpine/tests/clienttests/clienttools/start_service.sh
+++ os/alpine/tests/clienttests/clienttools/start_service.sh
@@ -23,19 +23,24 @@ jq . $serviceDefinition.content.template > $start_path/$serviceDefinition
 openssl pkcs12 -nocerts -out $SYSTEM_NAME-key.pem -in $cert_file
 openssl pkcs12 -clcerts -nokeys -out $SYSTEM_NAME.pem -in $cert_file
 
+chmod 777 $SYSTEM_NAME-key.pem
+chmod 777 $SYSTEM_NAME.pem 
+
 mv $SYSTEM_NAME-key.pem $start_path/$SYSTEM_NAME-key.pem 
 mv $SYSTEM_NAME.pem $start_path/$SYSTEM_NAME.pem 
 
 ## INIT START SERVER SCRIPT 
 sed "s/SYSTEM_NAME=\"...\"/SYSTEM_NAME=\"${SYSTEM_NAME}\"/" server.template > $start_file 
 sed -i "s%SYSTEM_PORT=...%SYSTEM_PORT=${SYSTEM_PORT}%" $start_file 
+sed -i "s%SERVER_PATH=\"...\"%SERVER_PATH=\"${start_path}\"%" $start_file 
 
+chmod +x $start_file
 mv $start_file $start_path
 
 ## INIT SERVER SERVICE FILE
 sed "s%start_file=\"...\"%start_file=\"${start_path}/${start_file}\"%" provider.service.template > $service_file 
 
-
+chmod +x $service_file
 mv $service_file /etc/init.d/ 
 rc-update add $service_file
 rc -s $service_file start
